e18dc9df155202aa523e90281f72917e9c380c1e
ruoyuangao <89848323+ruoyuangao@users.noreply.github.com>
2022-07-04 16:15:26 -0500

    photos model admin

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -20,7 +20,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializerWithProfile
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = (permissions.IsAdminUser,)
 
 class UserProfileViewSet(
     viewsets.GenericViewSet,
