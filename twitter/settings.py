5d0a6e2fd2d1bbbeabfb7e25cb51b1af981d666e
ruoyuangao <89848323+ruoyuangao@users.noreply.github.com>
2022-07-06 15:08:03 -0500

    add memcached

--- twitter/settings.py
+++ twitter/settings.py
@@ -171,6 +171,23 @@ AWS_S3_REGION_NAME = 'us-west-1'
 # - static usually store css, js these static codes, which user can visit directly
 # - media store the data files, not code
 MEDIA_ROOT = 'media/'
+# https://docs.djangoproject.com/en/3.1/topics/cache/
+# use `pip install python-memcached`
+# DO NOT pip install memcache or django-memcached
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 86400,
+    },
+    'testing': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 86400,
+        'KEY_PREFIX': 'testing',
+    },
+}
 
 try:
     from .local_settings import *
