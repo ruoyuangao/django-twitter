9a275f3f8423f029b8c18a28e90d069e16bfa23c
ruoyuangao <89848323+ruoyuangao@users.noreply.github.com>
2022-07-08 17:21:32 -0500

    cache news feed list in redis

--- newsfeeds/models.py
+++ newsfeeds/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+from newsfeeds.listeners import push_newsfeed_to_cache
 from tweets.models import Tweet
+from utils.memcached_helper import MemcachedHelper
 
 
 class NewsFeed(models.Model):
@@ -16,3 +20,10 @@ class NewsFeed(models.Model):
 
     def __str__(self):
         return f'{self.created_at} inbox of {self.user}: {self.tweet}'
+
+    @property
+    def cached_tweet(self):
+        return MemcachedHelper.get_object_through_cache(Tweet, self.tweet_id)
+
+
+post_save.connect(push_newsfeed_to_cache, sender=NewsFeed)
